59b8bcd359857210ca39f9169d420e47cbd950fb
yash22091 <34241504+yash22091@users.noreply.github.com>
2024-05-23 09:45:48 +0530

    Update wz-request.ts

--- plugins/main/public/react-services/wz-request.ts
+++ plugins/main/public/react-services/wz-request.ts
@@ -76,7 +76,7 @@ export class WzRequest {
             const wzMisc = new WzMisc();
             wzMisc.setApiIsDown(true);
             if (!window.location.hash.includes('#/settings')) {
-              window.location.href = getHttp().basePath.prepend('/app/wazuh#/health-check');
+              window.location.href = getHttp().basePath.prepend('/app/invinsense#/health-check');
             }
             throw new Error(error);
           }
